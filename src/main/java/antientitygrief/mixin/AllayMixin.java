9e82fb38d89810b806095bd04de29170ca4da3a7
BVengo <BVengo27@gmail.com>
2024-06-05 02:24:03 +1000

    Added Allay pickup

--- src/main/java/antientitygrief/mixin/AllayMixin.java
+++ src/main/java/antientitygrief/mixin/AllayMixin.java
@@ -1,4 +1,20 @@
 package antientitygrief.mixin;
 
+import antientitygrief.config.Capabilities;
+import antientitygrief.config.Configs;
+import net.minecraft.world.entity.animal.allay.Allay;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Allay.class)
 public class AllayMixin {
+    @Inject(method = "wantsToPickUp", at = @At("HEAD"), cancellable = true)
+    private void onWantsToPickUp(CallbackInfoReturnable<Boolean> cir) {
+        if(!Configs.ALLAY.canDo(Capabilities.PICKUP_ITEMS)) {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+    }
 }
