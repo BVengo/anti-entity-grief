b126ea3a3f042e678a73a93eab2bf2c48632105d
BVengo <BVengo27@gmail.com>
2024-06-04 19:34:55 +1000

    Handle egg trampling

---
+++ src/main/java/antientitygrief/mixin/TurtleEggBlockMixin.java
@@ -0,0 +1,21 @@
+package antientitygrief.mixin;
+
+import antientitygrief.AntiEntityGrief;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.TurtleEggBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(TurtleEggBlock.class)
+public class TurtleEggBlockMixin {
+    @Inject(method = "canDestroyEgg", at = @At("HEAD"), cancellable = true)
+    private void onCanDestroyEgg(Level level, Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if(!AntiEntityGrief.CONFIGS.getGriefingOption(entity)) {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+    }
+}
