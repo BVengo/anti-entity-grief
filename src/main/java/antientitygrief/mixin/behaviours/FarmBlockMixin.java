0a14fff6d1a8530a72035147b86f17893a054d6d
BVengo <bvengo27@gmail.com>
2024-08-30 10:37:08 +1000

    Split TRAMPLE_CROPS and TRAMPLE_FARMLAND
    Resolves #3

--- src/main/java/antientitygrief/mixin/behaviours/FarmBlockMixin.java
+++ src/main/java/antientitygrief/mixin/behaviours/FarmBlockMixin.java
@@ -1,11 +1,11 @@
 package antientitygrief.mixin.behaviours;
 
-import antientitygrief.AntiEntityGrief;
 import antientitygrief.config.Capabilities;
 import antientitygrief.config.Configs;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.FarmBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,15 +16,37 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(FarmBlock.class)
 public class FarmBlockMixin {
     @Inject(method = "turnToDirt", at = @At("HEAD"), cancellable = true)
-    private static void onTurnToDirt(Entity entity, BlockState state, Level level, BlockPos pos, CallbackInfo ci) {
-        // Prevent entities from turning farmland into dirt.
-        if(entity != null && !Configs.getGriefingOption(entity.getType(), Capabilities.TRAMPLE_FARMLAND)) {
-            // TODO: Check if the block above is a crop block. If TRAMPLE_CROPS is enabled, destroy the crops but
-            //  keep the farmland
+    private static void onTurnToDirt(Entity entity, BlockState blockState, Level level, BlockPos blockPos, CallbackInfo ci) {
+        /**
+         * Handle trampling of farmland and crops.
+         * - FC: Normal behaviour, since destruction of farmland and crops are both allowed. Trampling farmland will turn it into dirt and the crops will be dropped.
+         * - F : Cannot destroy crops. Will only turn farmland into dirt if it has no crops above it.
+         * - C : Cannot destroy farmland. Will still destroy crops when farmland is (normally) supposed to be turned into dirt.
+         */
 
-            // Alternatively, only have TRAMPLE_CROPS for ravager and the rest of the mobs have the default farmland
-            // trampling behaviour.
-            ci.cancel();
+        // Can only check with a valid entity.
+        if(entity == null) {
+            return;
         }
+
+        // Check block above for crop information.
+        BlockPos aboveBlockPos = blockPos.above();
+        BlockState aboveBlockState = level.getBlockState(aboveBlockPos);
+        boolean hasCrops = aboveBlockState.getBlock() instanceof CropBlock;
+
+        boolean canTrampleFarmland = Configs.getGriefingOption(entity.getType(), Capabilities.TRAMPLE_FARMLAND);
+        boolean canTrampleCrops = Configs.getGriefingOption(entity.getType(), Capabilities.TRAMPLE_CROPS);
+
+        if(canTrampleFarmland && (canTrampleCrops || !hasCrops)) {
+            // Has permission to turn this block to dirt. Also has permission to trample crops (or there are no crops).
+            return;
+        }
+        else if(canTrampleCrops && hasCrops) {
+            // Can't trample the farmland, but can trample crops. Destroy the crop block and drop items.
+            level.destroyBlock(aboveBlockPos, true);
+        }
+
+        // Unable to destroy the farmland.
+        ci.cancel();
     }
 }
